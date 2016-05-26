2987d6798a531d9b83ac0d9f8a920e1c410f6abe
pgorzelany <piotr.gorzelany@gmail.com>
2016-05-26 22:43:10 +0200

    Added toggle buttons to door menu

--- SwiftDemos/DoorMenuViewController.swift
+++ SwiftDemos/DoorMenuViewController.swift
@@ -1,4 +1,4 @@
-//
+    //
 //  DoorMenuViewController.swift
 //  SwiftDemos
 //
@@ -7,6 +7,12 @@
 //
 
 import UIKit
+
+private enum Side {
+    
+    case Left, Right
+    
+}
 
 class DoorMenuViewController: UIViewController, StoryboardInstantiable {
 
@@ -27,14 +33,35 @@ class DoorMenuViewController: UIViewController, StoryboardInstantiable {
     // MARK: Properties
     
     /** The width of the menu container as a percentage of the screen */
-    var menuWidth: CGFloat = 0.7
+    var menuWidth: CGFloat = 0.8
     
     private var maxAbsoluteContentTranslation: CGFloat {
         return UIScreen.mainScreen().bounds.width * self.menuWidth
     }
     
     /** The maximum angle (in degrees) the door menu can open */
-    var menuTilt: CGFloat = 20
+    private let menuTilt: CGFloat = 30
+    
+    /** Content translation at the beggining of the pan gesture */
+    private var initialContentTranslation: CGFloat = 0
+    
+    private var animating = false
+    
+    private let animationDuration: NSTimeInterval = 0.5
+    
+    private var isLeftMenuShown: Bool {
+        
+        return self.contentViewCenterConstraint.constant == self.maxAbsoluteContentTranslation
+        
+    }
+    
+    private var isRightMenuShown: Bool {
+        
+        return self.contentViewCenterConstraint.constant == -self.maxAbsoluteContentTranslation
+        
+    }
+    
+    
     
     // MARK: Initializers
     
@@ -51,11 +78,22 @@ class DoorMenuViewController: UIViewController, StoryboardInstantiable {
     
     // MARK: Actions
     
+    @IBAction func leftButtonTouched(sender: UIButton) {
+        
+        self.toggleLeftMenu()
+        
+    }
+    
+    @IBAction func rightButtonTouched(sender: UIButton) {
+        
+        self.toggleRightMenu()
+        
+    }
+    
     // MARK: Support
     
     private func configureController(){
         
-        print("menu width: \(self.maxAbsoluteContentTranslation)")
         self.leftMenuWidthConstraint.constant = self.maxAbsoluteContentTranslation
         self.rightMenuWidthConstraint.constant = self.maxAbsoluteContentTranslation
         let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panGestureRecognized))
@@ -66,11 +104,28 @@ class DoorMenuViewController: UIViewController, StoryboardInstantiable {
     func panGestureRecognized(recognizer: UIPanGestureRecognizer) {
         
         let translation = recognizer.translationInView(self.view)
-        self.translateContentView(inXDimension: translation.x)
+        print(translation)
+        
+        switch recognizer.state {
+            
+        case .Began:
+            
+            self.initialContentTranslation = self.contentViewCenterConstraint.constant
+            
+        case .Changed:
+            
+            self.translateContentView(inXDimension: translation.x + self.initialContentTranslation)
+            
+        default:
+            
+            break
+            
+        }
         
     }
     
-    private func translateContentView(inXDimension x: CGFloat) {
+    /** The absolute translation to make on the content view */
+    private func translateContentView(inXDimension x: CGFloat, animated: Bool = false) {
         
         guard abs(x) <= self.maxAbsoluteContentTranslation else {return}
         
@@ -85,11 +140,70 @@ class DoorMenuViewController: UIViewController, StoryboardInstantiable {
         var transform = CATransform3DIdentity;
         transform.m34 = 1.0 / -700;
         transform = CATransform3DRotate(transform, -relativeAngleTranslation * CGFloat(M_PI / 180.0), 0, 1, 0.0);
-
-        self.contentContainerView.layer.transform = transform;
         
+        if animated {
+            
+            UIView.animateWithDuration(self.animationDuration, animations: { 
+                
+                self.contentContainerView.layer.transform = transform;
+                self.view.layoutSubviews()
+                
+            })
+            
+        } else {
+            
+            self.contentContainerView.layer.transform = transform;
+            
+        }
 
     }
+    
+    private func showMenu(side: Side) {
+        
+        let translation = side == .Left ? self.maxAbsoluteContentTranslation : -self.maxAbsoluteContentTranslation
+        
+        self.translateContentView(inXDimension: translation, animated: true)
+        
+    }
+    
+    private func hideMenu() {
+        
+        self.contentViewCenterConstraint.constant = 0
+        
+        UIView.animateWithDuration(self.animationDuration) { 
+            
+            self.contentContainerView.layer.transform = CATransform3DIdentity
+            self.view.layoutSubviews()
+            
+        }
+        
+    }
+    
+    private func toggleMenu(side: Side) {
+        
+        if self.isLeftMenuShown || self.isRightMenuShown {
+            
+            self.hideMenu()
+            
+        } else {
+            
+            self.showMenu(side)
+        }
+        
+    }
+    
+    func toggleLeftMenu() {
+        
+        self.toggleMenu(.Left)
+        
+    }
+    
+    func toggleRightMenu() {
+        
+        self.toggleMenu(.Right)
+    }
+    
+    
     
     // MARK: Data
     
